9e6002a3b68c5316ed443e59185ffe2f85ba2fc1
Daniel Rees <daniel_james_rees@hotmail.com>
2022-05-06 20:22:02 -0400

    some basic empty routes and formatting

---
+++ src/routes/apps/index.svelte
@@ -0,0 +1,3 @@
+<div class="container mx-auto">
+	<h1 class="font-medium leading-tight text-5xl mt-0 mb-2">Steam Apps</h1>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mx-auto">
+	<h1 class="font-medium leading-tight text-5xl mt-0 mb-2">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+	<ul>
+		<li><a href="/apps">Apps</a></li>
+		<li><a href="/server">Servers</a></li>
+	</ul>
+</div>
